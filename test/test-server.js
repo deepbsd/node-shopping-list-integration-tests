7dde25d45beb5116d25f581ff7649d0b10f55a3a
David S. Jackson <deepbsd@yahoo.com>
2017-03-20 13:55:06 -0400

    added tests for /recipes endpoint--8 passing tests now

--- test/test-server.js
+++ test/test-server.js
@@ -62,6 +62,33 @@ describe('Shopping List', function() {
       });
   });
 
+
+  // GET tests for /recipes endpoint
+  it('should list items on GET', function() {
+    // for Mocha tests, when we're dealing with asynchronous operations,
+    // we must either return a Promise object or else call a `done` callback
+    // at the end of the test. The `chai.request(server).get...` call is asynchronous
+    // and returns a Promise, so we just return it.
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+
+        // because we create three items on app load
+        res.body.length.should.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `checked`.
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+
   // test strategy:
   //  1. make a POST request with data for a new item
   //  2. inspect response object and prove it has right
@@ -76,6 +103,24 @@ describe('Shopping List', function() {
         res.should.be.json;
         res.body.should.be.a('object');
         res.body.should.include.keys('id', 'name', 'checked');
+        res.body.id.should.not.be.null;
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+  // POST test for /recipes
+  it('should add an item on POST', function() {
+    const newItem = {name: 'ice water', ingredients: ['water', 'ice']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
         res.body.id.should.not.be.null;
         // response should be deep equal to `newItem` from above if we assign
         // `id` to it from `res.body.id`
@@ -124,6 +169,41 @@ describe('Shopping List', function() {
       });
   });
 
+  //   PUT request for /recipes endpoint
+  it('should update items on PUT', function() {
+      // we initialize our updateData here and then after the initial
+      // request to the app, we update it with an `id` property so
+      // we can make a second, PUT call to the app.
+      const updateData = {
+        name: 'ice water',
+        ingredients: ['ice', 'water', 'glass']
+      };
+
+
+    return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        // this will return a promise whose value will be the response
+        // object, which we can inspect in the next `then` back. Note
+        // that we could have used a nested callback here instead of
+        // returning a promise and chaining with `then`, but we find
+        // this approach cleaner and easier to read and reason about.
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      // prove that the PUT request has right status code
+      // and returns updated item
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.deep.equal(updateData);
+      });
+  });
+
   // test strategy:
   //  1. GET a shopping list items so we can get ID of one
   //  to delete.
@@ -141,4 +221,19 @@ describe('Shopping List', function() {
         res.should.have.status(204);
       });
   });
+
+  // DELETE request for /recipes
+  it('should delete items on DELETE', function() {
+  return chai.request(app)
+    // first have to get so we have an `id` of item
+    // to delete
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+        .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res) {
+      res.should.have.status(204);
+    });
+});
 });
